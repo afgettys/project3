10d5c4f6c07332d92c74599ba204b510307322e2
Daniel Prada <daniel@lamusica.com>
2020-09-10 12:57:21 -0400

    Adding tweaks to show how login/logout flow works

--- client/src/App.js
+++ client/src/App.js
@@ -8,10 +8,7 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import discussion from "./pages/discussion";
 
-
-
-
-function App() {
+function App () {
 
   return (
     <Router>
